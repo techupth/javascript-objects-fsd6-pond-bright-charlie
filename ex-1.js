c7bdda2cbe47c083b22e6a619ccae4a64da558d2
Pond <pondwachi.th@gmail.com>
2024-05-10 13:34:14 +0700

    feat: Js work  of pond

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,17 @@
 // Exercise #1: Food Order
 
 // Start coding here
+
+let foodOrder = {
+    orderNumber: "A0234",
+    adress: "Bangkok",
+    paymentType: "cash",
+    totalPrice: 4500,
+    restaurantName: "MK"
+
+}
+
+foodOrder.totalPrice = 5000
+foodOrder.paymentType = "Credit Card"
+
+console.log(foodOrder)
